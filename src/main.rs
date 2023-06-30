eb7ffa11ac6d3aa755c2b623f5b1e4df59e344d8
izeeyahmon <95024418+izeeyahmon@users.noreply.github.com>
2023-06-30 22:28:16 +0800

    made it to a loop as advised by pweh

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use ethers::{
 //use std::{fs::File, io::{Read, Write}};
 use eyre::Result;
 use std::sync::Arc;
+use std::{thread, time::Duration};
 mod addwebhook;
 use addwebhook::send_webhook;
 abigen!(
@@ -34,60 +35,62 @@ const UNISWAP_FACTORY: &str = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f";
 const WETH_ADDRESS: &str = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2";
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let provider = Provider::<Http>::try_from(HTTP_URL)?;
-    let provider = Arc::new(provider);
-    let block_number: U64 = provider.get_block_number().await?;
-    // let mut file = File::open("blocknumber.txt")?;
-    // let mut contents = String::new();
-    // file.read_to_string(&mut contents)?;
-    // if contents == block_number.to_string(){
-    //     println!("We are polling the same block");
-    //     std::process::exit(1);
+    loop {
+        let provider = Provider::<Http>::try_from(HTTP_URL).unwrap();
+        let provider = Arc::new(provider);
+        let block_number: U64 = provider.get_block_number().await.unwrap();
+        // let mut file = File::open("blocknumber.txt").unwrap();
+        // let mut contents = String::new();
+        // file.read_to_string(&mut contents).unwrap();
+        // if contents == block_number.to_string(){
+        //     println!("We are polling the same block");
+        //     std::process::exit(1);
 
-    // }
-    // TO-DO  - get proper error propagation over at functions and refactor below If statements ,
-    // this looks to be sendable via a function
-    let address: Address = UNISWAP_FACTORY.parse()?;
-    let filter = Filter::new()
-        .address(address)
-        .event("PairCreated(address,address,address,uint256)")
-        .from_block(block_number) ;
-    let logs = provider.get_logs(&filter).await?;
-    for log in logs.iter() {
-        let token0 = Address::from(log.topics[1]);
-        let token1 = Address::from(log.topics[2]);
-        let weth_address: Address = WETH_ADDRESS.parse()?;
-        let pairadd = Address::from(&log.data[12..32].try_into()?);
-        let pair_contract = UniswapPair::new(pairadd, provider.clone());
-        if token0 == weth_address {
-            let liq = pair_contract.get_reserves().await?;
-            let liq_0 = format_units(U256::from(liq.0), "ether").unwrap();
-            let token1_contract = ERC20::new(token1, provider.clone());
-            let token1_symbol: String = token1_contract.symbol().await?;
-            println!(
-                "New Pair Detected {:?}/WETH at {:?} with {:?}ETH pooled ETH",
-                &token1_symbol, pairadd, liq_0
-            );
-            if send_webhook(token1_symbol.clone(), pairadd, liq_0.clone())
-                .await
-                .is_ok()
-            {
-                println!("Webhook sent");
+        // }
+        // TO-DO  - get proper error propagation over at functions and refactor below If statements ,
+        // this looks to be sendable via a function
+        let address: Address = UNISWAP_FACTORY.parse().unwrap();
+        let filter = Filter::new()
+            .address(address)
+            .event("PairCreated(address,address,address,uint256)")
+            .from_block(block_number);
+        let logs = provider.get_logs(&filter).await.unwrap();
+        println!("{:?}", block_number);
+        for log in logs.iter() {
+            let token0 = Address::from(log.topics[1]);
+            let token1 = Address::from(log.topics[2]);
+            let weth_address: Address = WETH_ADDRESS.parse().unwrap();
+            let pairadd = Address::from(&log.data[12..32].try_into()?);
+            let pair_contract = UniswapPair::new(pairadd, provider.clone());
+            if token0 == weth_address {
+                let liq = pair_contract.get_reserves().await.unwrap();
+                let liq_0 = format_units(U256::from(liq.0), "ether").unwrap();
+                let token1_contract = ERC20::new(token1, provider.clone());
+                let token1_symbol: String = token1_contract.symbol().await.unwrap();
+                println!(
+                    "New Pair Detected {:?}/WETH at {:?} with {:?}ETH pooled ETH",
+                    &token1_symbol, pairadd, liq_0
+                );
+                if send_webhook(token1_symbol.clone(), pairadd, liq_0.clone())
+                    .await
+                    .is_ok()
+                {
+                    println!("Webhook sent");
+                }
+            } else {
+                let liq = pair_contract.get_reserves().await.unwrap();
+                let liq_1 = format_units(U256::from(liq.1), "ether").unwrap();
+                let token0_contract = ERC20::new(token0, provider.clone());
+                let token0_symbol: String = token0_contract.symbol().await.unwrap();
+                println!(
+                    "New Pair Detected {:?}/WETH at {:?} with {:?}ETH pooled ETH",
+                    &token0_symbol, pairadd, liq_1
+                );
+                send_webhook(token0_symbol.clone(), pairadd, liq_1.clone())
+                    .await
+                    .unwrap();
             }
-        } else {
-            let liq = pair_contract.get_reserves().await?;
-            let liq_1 = format_units(U256::from(liq.1), "ether").unwrap();
-            let token0_contract = ERC20::new(token0, provider.clone());
-            let token0_symbol: String = token0_contract.symbol().await?;
-            println!(
-                "New Pair Detected {:?}/WETH at {:?} with {:?}ETH pooled ETH",
-                &token0_symbol, pairadd, liq_1
-            );
-            send_webhook(token0_symbol.clone(), pairadd, liq_1.clone()).await?;
         }
+        thread::sleep(Duration::from_secs(8));
     }
-    //let mut f = std::fs::OpenOptions::new().write(true).truncate(true).open("blocknumber.txt")?;
-    // f.write_all(block_number.to_string().as_bytes())?;
-    // f.flush()?;
-    Ok(())
 }
